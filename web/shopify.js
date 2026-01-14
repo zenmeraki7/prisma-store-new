be2fef5756983cb1024f35542728d45a628c5be2
zenmeraki7 <zenmeraki7>
2026-01-14 11:07:24 +0530

    errors

--- web/shopify.js
+++ web/shopify.js
@@ -1,29 +1,16 @@
-import { BillingInterval, LATEST_API_VERSION } from "@shopify/shopify-api";
+import { BillingInterval } from "@shopify/shopify-api";
 import { shopifyApp } from "@shopify/shopify-app-express";
 import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prisma";
-import { restResources } from "@shopify/shopify-api/rest/admin/2024-10";
 import prisma from "../lib/prisma.server.js";
 
 const shopify = shopifyApp({
   api: {
-    apiVersion: LATEST_API_VERSION,
-    restResources,
-    future: {
-      customerAddressDefaultFix: true,
-      lineItemBilling: true,
-      unstable_managedPricingSupport: true,
-    },
-    billing: undefined,
+    apiKey: process.env.SHOPIFY_API_KEY,
+    apiSecretKey: process.env.SHOPIFY_API_SECRET,
+    apiVersion: "2024-10", // ✅ FIX
+    scopes: process.env.SCOPES?.split(","),
+    hostName: process.env.HOST.replace(/^https?:\/\//, ""),
   },
-  auth: {
-    path: "/api/auth",
-    callbackPath: "/api/auth/callback",
-  },
-  webhooks: {
-    path: "/api/webhooks",
-  },
-
-  // ✅ PRISMA SESSION STORAGE (Supabase)
   sessionStorage: new PrismaSessionStorage(prisma),
 });
 
